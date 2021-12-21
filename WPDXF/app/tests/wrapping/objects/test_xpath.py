e36652173f9eea70b3c82c6184095ba4de6dda10
Fabian J <johannsen.fabian@web.de>
2021-12-21 11:13:27 +0100

    Test: XPathNode

--- WPDXF/app/tests/wrapping/objects/test_xpath.py
+++ WPDXF/app/tests/wrapping/objects/test_xpath.py
@@ -1,28 +1,31 @@
-from wrapping.objects.xpath.predicate import AttributePredicate, Conjunction, Disjunction, Predicate
+from lxml.etree import fromstring, tostring
+from wrapping.objects.xpath.node import AXISNAMES, XPathNode, get_position
+from wrapping.objects.xpath.predicate import (AttributePredicate, Conjunction,
+                                              Disjunction, Predicate)
 
 
 def test_predicates():
-    predicate0 = Predicate(left='position()')
+    predicate0 = Predicate(left="position()")
     target = "position()"
     assert target == str(predicate0)
 
-    predicate1 = Predicate(left='position()', comp=None, right=None)
+    predicate1 = Predicate(left="position()", comp=None, right=None)
     target = "position()"
     assert target == str(predicate1)
 
     assert predicate0 == predicate1
 
-    predicate1 = Predicate(left='position()', comp=None, right=1)
+    predicate1 = Predicate(left="position()", comp=None, right=1)
     target = "1"
     assert target == str(predicate1)
 
     assert predicate0 != predicate1
 
-    predicate0 = AttributePredicate(left='key')
+    predicate0 = AttributePredicate(left="key")
     target = "@key"
     assert target == str(predicate0)
 
-    predicate0 = AttributePredicate(left='key', right='value')
+    predicate0 = AttributePredicate(left="key", right="value")
     target = '@key="value"'
     assert target == str(predicate0)
 
@@ -31,7 +34,7 @@ def test_predicates():
     assert target == str(conjunction)
 
     conjunction = Conjunction([Predicate(left="p0"), Predicate(left="p1")])
-    target= "[p0][p1]"
+    target = "[p0][p1]"
     assert target == str(conjunction)
 
     disjunction = Disjunction([])
@@ -39,9 +42,81 @@ def test_predicates():
     assert target == str(disjunction)
 
     disjunction = Disjunction([Predicate(left="p0"), Predicate(left="p1")])
-    target= "p0 or p1"
+    target = "p0 or p1"
     assert target == str(disjunction)
 
-    conjunction = Conjunction([Disjunction([Predicate("p0"), Predicate("p1")]), Predicate(left="p1")])
-    target= "[p0 or p1][p1]"
+    conjunction = Conjunction(
+        [Disjunction([Predicate("p0"), Predicate("p1")]), Predicate(left="p1")]
+    )
+    target = "[p0 or p1][p1]"
     assert target == str(conjunction)
+
+
+def test_get_position():
+    html = fromstring("<div><a>First a</a><b>First b</b><a>Second a</a></div>")
+    a0_element = html.find("a")
+    a1_element = html.findall("a")[-1]
+
+    target = 1
+    output = get_position(html)
+    assert target == output
+
+    target = 1
+    output = get_position(a0_element)
+    assert target == output
+
+    target = 2
+    output = get_position(a1_element)
+    assert target == output
+
+
+def test_nodes():
+    node = XPathNode()
+    assert node.axisname is AXISNAMES.CHLD
+    assert node.nodetest == "node()"
+    assert isinstance(node.predicates, Conjunction)
+    target = "*"
+    assert target == str(node)
+
+    node.nodetest = "div"
+    target = "div"
+    assert target == str(node)
+
+    node.axisname = AXISNAMES.DESC
+    target = "descendant::div"
+    assert target == str(node)
+
+    node.predicates.append(Predicate("key"))
+    target = "descendant::div[key]"
+    assert target == str(node)
+
+    html = fromstring("<div><a>First a</a><b>First b</b><a>Second a</a></div>")
+
+    target = html
+    # input
+    step = XPathNode.new_instance(html)
+    xpath = "/" + str(step)
+
+    output = html.xpath(xpath)
+    assert len(output) == 1
+    output = output[0]
+    assert target == output
+
+    target = html.getchildren()[0]
+    # input
+    step = XPathNode.new_instance(target)
+    xpath += "/" + str(step)
+
+    output = html.xpath(xpath)
+    assert len(output) == 1
+    output = output[0]
+    assert target == output
+
+    # input
+    step = XPathNode.new_self()
+    xpath += "/" + str(step)
+
+    output = html.xpath(xpath)
+    assert len(output) == 1
+    output = output[0]
+    assert target == output
--- WPDXF/app/wrapping/objects/xpath/node.py
+++ WPDXF/app/wrapping/objects/xpath/node.py
@@ -1,33 +1,77 @@
-from lxml.etree import ElementBase
+from enum import Enum
+
+from lxml.etree import ElementBase, _Element
 from wrapping.objects.xpath.predicate import Conjunction, Predicate
 
 
-def get_position(element):
-    p = element.getparent()
-    if p is None:
-        return 1
-    equal_siblings = [ch for ch in p.getchildren() if ch.tag == element.tag]
-    # position = 0 indicates, that the element is unique and position value can be omitted.
-    # if len(equal_siblings) == 1:
-    #     return 0
-    return equal_siblings.index(element) + 1
+class AXISNAMES(str, Enum):
+    ANC = "ancestor"
+    ANOS = "ancestor-or-self"
+    ATTR = "attribute"
+    CHLD = "child"
+    DESC = "descendant"
+    DEOS = "descendant-or-self"
+    FOLW = "following"
+    FSIB = "following-sibling"
+    NS = "namespace"
+    PAR = "parent"
+    PREC = "preceding"
+    PSIB = "preceding-sibling"
+    SELF = "self"
+
+
+def get_position(element: _Element) -> int:
+    """Returns the position of an element with respect to all siblings with the same tag.
+    Value returns the position() predicate of the element
+
+    Args:
+        element (_Element): Target element
+
+    Returns:
+        int: Position of 'element' (position > 1).
+    """
+    return int(element.xpath(f"count(preceding-sibling::{element.tag})")) + 1
 
 
 class XPathNode:
-    def __init__(self, *, axisname="child", nodetest="node()", predicates=None) -> None:
-        self.axisname = axisname
-        self.nodetest = nodetest
+    """Wrapper for a single step (node) in an XPath.
+    Maintains corresponding attributes and simplifies XPath representation.
+    """
+
+    def __init__(
+        self,
+        *,
+        axisname: AXISNAMES = None,
+        nodetest: str = None,
+        predicates: Conjunction = None,
+    ) -> None:
+        """A single step of an XPath. This is adapted from the W3C representation.
+        step := axisname::nodetest[predicates]
+        See: https://www.w3.org/TR/1999/REC-xpath-19991116/#section-Location-Steps
+
+        In this case, Predicates are considered to be in conjunctive normal form (CNF).
+        It is not checked whether this is true or not.
+        _predicates is used to store "meta predicates" that should not be considered nor changed
+        in any calculation (e.g. the relative start_path).
+
+        Args:
+            axisname (AXISNAMES, optional): The axis to be considered. Defaults to "child".
+            nodetest (str, optional): Specifies the node test if necessary. Defaults to "node()".
+            predicates (Conjunction, optional): Predicates in CNF. Defaults to Conjunction().
+        """
+        self.axisname = axisname or AXISNAMES.CHLD
+        self.nodetest = nodetest or "node()"
         self.predicates = predicates or Conjunction()
         self._predicates = Conjunction()
 
-    def __repr__(self) -> str:
-        predicates = self.predicates + self._predicates 
+    def __str__(self) -> str:
+        predicates = self.predicates + self._predicates
         if not predicates:
-            if self.axisname == "descendant-or-self":
+            if self.axisname is AXISNAMES.DEOS:
                 return ""
-            if self.axisname == "self":
+            if self.axisname is AXISNAMES.SELF:
                 return "."
-            if self.axisname == "parent":
+            if self.axisname is AXISNAMES.PAR:
                 return ".."
 
         if self.nodetest == "node()":
@@ -35,12 +79,22 @@ class XPathNode:
         else:
             nodetest = self.nodetest
 
-        if self.axisname == "child":
+        if self.axisname is AXISNAMES.CHLD:
             return nodetest + str(predicates)
         return f"{self.axisname}::{nodetest}" + str(predicates)
 
     @staticmethod
-    def new_instance(step: ElementBase):
+    def new_instance(step: _Element):
+        """Returns a new instance based on an lxml.etree._Element.
+        The resulting XPathNode matches the 'step' in terms of nodetest and predicate position().
+        """
         context = XPathNode(nodetest=step.tag)
         context.predicates.append(Predicate("position()", right=get_position(step)))
         return context
+
+    @staticmethod
+    def new_self(nodetest=None, predicates=None):
+        # Shortcut to get a self-referencing step.
+        return XPathNode(
+            axisname=AXISNAMES.SELF, nodetest=nodetest, predicates=predicates
+        )
