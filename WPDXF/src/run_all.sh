e59b9dcb182bc115d10ebfeb7ab60c33ea8483ab
Fabian J <johannsen.fabian@web.de>
2022-03-02 15:22:22 +0100

    Updated run all

--- WPDXF/src/run_all.sh
+++ WPDXF/src/run_all.sh
@@ -1,5 +1,10 @@
 for file in $1*
 do
     echo $file
-    python main.py -b "$file" -m $2 --num_examples $3 --num_queries $4 --tau $5
+    if [$6 == "1"]
+    then
+        python main.py -b "$file" -m $2 --num_examples $3 --num_queries $4 --tau $5 --enrich_predicates
+    else
+        python main.py -b "$file" -m $2 --num_examples $3 --num_queries $4 --tau $5 
+    fi
 done
