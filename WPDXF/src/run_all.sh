c8c1e207b7df4f876e5d9fed62e6b58e58dd9d29
Fabian J <johannsen.fabian@web.de>
2022-03-02 15:29:30 +0100

    runall fix

--- WPDXF/src/run_all.sh
+++ WPDXF/src/run_all.sh
@@ -1,7 +1,7 @@
 for file in $1*
 do
     echo $file
-    if [$6 == "1"]
+    if [ "$6" == "1" ];
     then
         python main.py -b "$file" -m $2 --num_examples $3 --num_queries $4 --tau $5 --enrich_predicates
     else
